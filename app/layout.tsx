f6dcfe22a44bed1effba0fc446805f761d3e4feb
Stanchev <59915249+mwolfyy@users.noreply.github.com>
2025-06-07 11:12:31 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Card Time',
+  description: 'AI скенер, който превръща визитките в реалност!',
+  author: 'Станчев | Stanchev',
 }
 
 export default function RootLayout({
